8328f491870c5e7eab910df06fa6b9b49b1b53d2
Paula Fenner <fenner.paula@gmail.com>
2025-02-28 12:07:30 +0100

    Implemented the examples section.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
-import { CORE_CONCEPTS } from '../data';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from '../data';
 import CoreConcept from './CoreConcept';
 import TabButton from './TabButton';
 
 export default function Main() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
   return (
     <main>
       <section id="core-concepts">
@@ -21,8 +29,30 @@ export default function Main() {
       <section id="examples">
         <h2>Examples</h2>
         <menu>
-          <TabButton>Components</TabButton>
+          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleClick('components')}>
+            Components
+          </TabButton>
+          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleClick('jsx')}>
+            JSX
+          </TabButton>
+          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleClick('props')}>
+            Props
+          </TabButton>
+          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleClick('state')}>
+            State
+          </TabButton>
         </menu>
+        {!selectedTopic ? (
+          <p>Please select a topic.</p>
+        ) : (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        )}
       </section>
     </main>
   );
